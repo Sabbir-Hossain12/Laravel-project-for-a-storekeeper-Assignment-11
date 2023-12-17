4aab2d57bb17aebf25bd9e30189561b25c961f8a
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2023-12-17 11:14:33 +0600

    1st phase

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\resourceController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/home',[homeController::class,'home']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::resource('/store',resourceController::class)->only(['index','store']);
